9a7cf0405a06ef674c99f08b5650124bd5bd32b3
Shlomihaser <hasshlomi21@gmail.com>
2025-06-11 22:06:07 +0300

    Initial commit - analytics library

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "analytics-sdk-android"
+include(":app")
+include(":analytics-sdk-and")
